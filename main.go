9d34be920bace8e81959bcc599ff5a57f08b7eff
byzbl <850025847@qq.com>
2018-10-12 09:29:45 +0800

    111

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("111hello git!")
+
+	type S struct {
+		F string `species:"gopher" color:"blue"`
+	}
+
+	s := S{}
+	st := reflect.TypeOf(s)
+	field, _ := st.FieldByName("F")
+	//field := st.Field(0)
+	fmt.Println(field.Tag.Get("color"), field.Tag.Get("species"))
+	//fmt.Println("111hello git!")
 }
